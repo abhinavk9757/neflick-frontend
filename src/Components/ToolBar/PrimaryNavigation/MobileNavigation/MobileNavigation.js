32d7d51c6dab6a3f5daf45e15c603687ee852edd
Abhinav <abhinav95@hotmail.com>
2018-12-31 17:49:15 +0530

    Primary Navigation Mobile

---
+++ src/Components/ToolBar/PrimaryNavigation/MobileNavigation/MobileNavigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const mobileNavigation = () => {
+    return (
+    <div>
+
+    </div>);
+}
+
+export default mobileNavigation;
--- src/Components/ToolBar/PrimaryNavigation/PrimaryNavigation.js
+++ src/Components/ToolBar/PrimaryNavigation/PrimaryNavigation.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import classes from './PrimaryNavigation.module.css';
 
-const primaryNavigation = () => {
-  return (
-    <div className={classes.PrimaryNavigation}>
-      <div className={classes.DesktopOnly}>
-        <a href="/">Home</a>
-        <a href="/">Series</a>
-        <a href="/">Film</a>
-        <a href="/">Recently Added</a>
-        <a href="/">My List</a>
+class PrimaryNavigation extends React.Component {
+  state = {
+    display: 'none',
+  };
+  mobileLinksHandler = () => {
+    if (this.state.display === 'none') {
+      this.setState({ display: 'flex' });
+    } else {
+      this.setState({ display: 'none' });
+    }
+  };
+  render() {
+    return (
+      <div className={classes.PrimaryNavigation}>
+        <div className={classes.DesktopOnly}>
+          <a href="/">Home</a>
+          <a href="/">Series</a>
+          <a href="/">Film</a>
+          <a href="/">Recently Added</a>
+          <a href="/">My List</a>
+        </div>
+        <div className={classes.MobileOnly} onClick={this.mobileLinksHandler}>
+          <p>Browse</p>
+        </div>
+        <div
+          name="MobileLinks"
+          className={classes.MobileLinks}
+          style={{ display: this.state.display }}
+        >
+          <div>Home</div>
+          <div>Series</div>
+          <div>Film</div>
+          <div>Recently Added</div>
+          <div>My List</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default primaryNavigation;
+export default PrimaryNavigation;
